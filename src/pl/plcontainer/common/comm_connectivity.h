20a6f376c5bcef4c7284648d8d28a046424c6c9e
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-02-26 22:32:19 +0000

    Committing missed connectivity code

---
+++ src/pl/plcontainer/common/comm_connectivity.h
@@ -0,0 +1,37 @@
+#ifndef COMM_CONNECTIVITY_H
+#define COMM_CONNECTIVITY_H
+
+#include <stdio.h>
+
+#define PLC_BUFFER_SIZE 8192
+#define PLC_BUFFER_MIN_FREE 1000
+#define PLC_INPUT_BUFFER 0
+#define PLC_OUTPUT_BUFFER 1
+
+struct plc_buffer {
+    char *data;
+    int pStart;
+    int pEnd;
+    int bufSize;
+};
+
+typedef struct plc_buffer plcBuffer;
+
+struct plc_conn {
+    int sock;
+    plcBuffer* buffer[2];
+    FILE *fDebug;
+};
+
+typedef struct plc_conn plcConn;
+
+plcConn * plcConnect(int port);
+plcConn * plcConnInit(int sock);
+void plcDisconnect(plcConn *conn);
+int plcBufferAppend (plcConn *conn, char *prt, size_t len);
+int plcBufferRead (plcConn *conn, char *resBuffer, size_t len);
+int plcBufferReceive (plcConn *conn, size_t nBytes);
+int plcBufferFlush (plcConn *conn);
+void plcConnectionDebug(plcConn *conn, FILE* f);
+
+#endif /* COMM_CONNECTIVITY_H */
