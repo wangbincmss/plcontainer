a3d04223c0a77995703f3e53ec8c898c39f9d0ce
Hubert Zhang <hubertzhang@apache.org>
2017-08-03 07:58:14 +0800

    Fix plc client build

--- concourse/scripts/plcontainer_clients_build.sh
+++ concourse/scripts/plcontainer_clients_build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -x
+set -e -x
+
+# Install R
+yum -y install R R-devel R-core
 
 # Clean up and creating target directories
 rm -rf plcontainer_clients_build/python26 && mkdir plcontainer_clients_build/python26
 rm -rf plcontainer_clients_build/python27 && mkdir plcontainer_clients_build/python27
-rm -rf plcontainer_clients_build/python34 && mkdir plcontainer_clients_build/python34
-rm -rf plcontainer_clients_build/python35 && mkdir plcontainer_clients_build/python35
-rm -rf plcontainer_clients_build/r31 && mkdir plcontainer_clients_build/r31
 rm -rf plcontainer_clients_build/r32 && mkdir plcontainer_clients_build/r32
+rm -rf plcontainer_clients_build/r31 && mkdir plcontainer_clients_build/r31
 
 # Build Python 2.7 and R 3.2 - default ones
 cd plcontainer_src
@@ -25,32 +26,14 @@ cp src/rclient/librcall.so ../plcontainer_clients_build/r32/librcall.so
 unset R_HOME
 
 # Build Python 2.6 and R 3.1 - the ones shipped with GPDB
-cp bin_gpdb4off_centos7/bin_gpdb4off_centos7.tar.gz /usr/local
-pushd /usr/local
+mkdir /usr/local/greenplum-db
+cp bin_gpdb/bin_gpdb.tar.gz /usr/local/greenplum-db/
+pushd /usr/local/greenplum-db/
 tar zxvf $1.tar.gz
 popd
 source /usr/local/greenplum-db/greenplum_path.sh
 pushd src/pyclient
 make clean && make
 popd
-pushd src/rclient
-make clean && make
-popd
 cp src/pyclient/client     ../plcontainer_clients_build/python26/client
-cp src/rclient/client      ../plcontainer_clients_build/r31/client
-cp src/rclient/librcall.so ../plcontainer_clients_build/r31/librcall.so
 
-# Build Python 3.4 and Python 3.5
-export PYTHON=python3
-pushd src/pyclient
-make clean && make
-popd
-cp src/pyclient/client    ../plcontainer_clients_build/python34/client
-unset PYTHON
-export LD_LIBRARY_PATH=/usr/local/anaconda/lib
-export PYTHONHOME=/usr/local/anaconda
-export PATH=/usr/local/anaconda/bin:$PATH
-pushd src/pyclient
-make clean && make
-popd
-cp src/pyclient/client    ../plcontainer_clients_build/python35/client
