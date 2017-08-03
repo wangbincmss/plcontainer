34a8b1be6626a8e7b56b6249989ff3f5040ce5b6
Hubert Zhang <hubertzhang@apache.org>
2017-08-03 20:21:53 +0800

    d

--- concourse/scripts/plcontainer.sh
+++ concourse/scripts/plcontainer.sh
@@ -3,8 +3,8 @@
 set -eox pipefail
 
 ccp_src/aws/setup_ssh_to_cluster.sh
-plcontainer_src/concourse/script/docker_install.sh
-plcontainer_src/concourse/script/docker_build.sh
-plcontainer_src/concourse/script/run_plcontainer.sh
+plcontainer_src/concourse/scripts/docker_install.sh
+plcontainer_src/concourse/scripts/docker_build.sh
+plcontainer_src/concourse/scripts/run_plcontainer.sh
 
 
