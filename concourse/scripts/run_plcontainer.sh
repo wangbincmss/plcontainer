45fa79b3c3d43b1cf613bb1b2df6f90e205ef40d
Hubert Zhang <hubertzhang@apache.org>
2017-08-04 11:47:57 +0800

    a

--- concourse/scripts/run_plcontainer.sh
+++ concourse/scripts/run_plcontainer.sh
@@ -8,10 +8,9 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh; \
 gppkg -i plcontainer_gpdb_build/plcontainer-concourse-centos6.gppkg; \
 plcontainer-config --reset; \
 gpstop -arf; \
-psql -d postgres -c \"create database pltest\"; \
-psql -d pltest -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
-psql -d pltest -f plcontainer_src/concourse/scripts/function_setup.sql; \
-psql -d pltest -f plcontainer_src/concourse/scripts/perf_prepare1.sql; \
-nohup psql -d pltest -f plcontainer_src/concourse/scripts/perf_pl1.sql > perf_pl1 2>&1; \
-nohup psql -d pltest -f plcontainer_src/concourse/scripts/perf_py1.sql > perf_py1 2>&1; \
+psql -d postgres -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
+psql -d postgres -f plcontainer_src/concourse/scripts/function_setup.sql; \
+psql -d postgres -f plcontainer_src/concourse/scripts/perf_prepare1.sql; \
+nohup psql -d postgres -f plcontainer_src/concourse/scripts/perf_pl1.sql > perf_pl1 2>&1; \
+nohup psql -d postgres -f plcontainer_src/concourse/scripts/perf_py1.sql > perf_py1 2>&1; \
 \""
