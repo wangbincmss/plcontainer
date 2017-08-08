96b5d2e94893f49180e62122619fb797a7c6757f
Hubert Zhang <hubertzhang@apache.org>
2017-08-08 17:18:17 +0800

    a

--- concourse/scripts/run_plcontainer.sh
+++ concourse/scripts/run_plcontainer.sh
@@ -19,3 +19,4 @@ nohup psql -d postgres -f plcontainer_src/concourse/scripts/perf_py1.sql > perf_
 
 scp mdw:~/perf_pl1 plcontainer_perf_result/perf_pl1
 scp mdw:~/perf_py1 plcontainer_perf_result/perf_py1
+exit 1
